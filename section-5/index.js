9409bcc0f03559401305ebd46297fd58dbf30746
MANISH-GIRI <manish.giri.me@gmail.com>
2020-06-18 19:31:51 -0400

    Run multiple containers with Docker Compose

--- section-5/index.js
+++ section-5/index.js
@@ -1,3 +1,21 @@
 const express = require('express');
-const app = express();
+const redis = require('redis');
 
+const app = express();
+const PORT = 8080;
+const client = redis.createClient({
+    host: 'redis-server', // name of the docker service
+    port: 6379
+});
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    // fetch num of visits
+    client.get('visits', (err, visits) => {
+        res.send(`<h1 style='color: brown'>Number of visits: <span style='color: green'> ${visits}</span></h1>`);
+        client.set('visits', +visits + 1);
+    });
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
